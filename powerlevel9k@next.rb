a7bbc178df593c7fd0ec65bbb68734c5fc0df3e7
Tim Otlik <t.otlik@vivaldi.net>
2018-11-12 20:15:47 +0100

    feat: add formula for next branch

---
+++ powerlevel9k@next.rb
@@ -0,0 +1,20 @@
+class Powerlevel9kATnext < Formula
+  desc "A badass zsh theme with more power than a normal earthling"
+  homepage "https://github.com/bhilburn/powerlevel9k"
+  url "https://github.com/bhilburn/powerlevel9k/archive/next.tar.gz"
+
+  depends_on "zsh" => "5.1"
+
+  def install
+    (var/"log/powerlevel9k").mkpath
+    prefix.install Dir["*"]
+  end
+
+  def caveats; <<~EOS
+    If you want to load powerlevel9k in your zsh simply add the following line to your .zshrc:
+      
+      source #{opt_prefix}/powerlevel9k.zsh-theme
+    
+    EOS
+  end
+end
